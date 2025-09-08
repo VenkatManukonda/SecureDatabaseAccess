2ba462b44d8764043489c89276d5ddb195bb1e4f
VenkatManukonda <vmanukonda43633@ucumberlands.edu>
2025-09-07 20:24:42 -0500

    Create SecureDatabaseApplication.java

---
+++ SecureDatabaseApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class SecureDatabaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecureDatabaseApplication.class, args);
+    }
+}
