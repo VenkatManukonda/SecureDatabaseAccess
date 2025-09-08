736e6b253da71e0eac3f2ee287382074356baa6c
VenkatManukonda <vmanukonda43633@ucumberlands.edu>
2025-09-07 20:41:10 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,11 @@
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
